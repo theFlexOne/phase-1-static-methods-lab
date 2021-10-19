1ced36802a2fc19b1b5e790bb88d39f757648412
Eric Hafvenstein <edhavey@null.net>
2021-10-19 16:04:09 -0500

    1 & 2 done. Middle of 3rd

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(word) {
+    return word[0].toUpperCase() + word.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.split("").filter(char => char.match(/[a-zA-Z0-9' -]/)).join("");
+  }
+
+  static titleize(string) {
+    const EXCEPTIONS = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return string.split(" ").map((word, i) => {
+      if (i === 0) return Formatter.capitalize(word);
+      else if (EXCEPTIONS.some((exception) => word === exception)) return word;
+      else return Formatter.capitalize(word);
+    })
+  }
 }
