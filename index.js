8279fdc57082cfa82b07cf8b9b596b5a38e1285b
theflexone <edhavey@null.net>
2021-10-19 21:48:04 -0500

    assignment complete

--- index.js
+++ index.js
@@ -5,17 +5,28 @@ const capitalizeWord = (word, i) => {
 };
 
 class Formatter {
-  
   static capitalize(word) {
     return word[0].toUpperCase() + word.slice(1);
   }
-
   static sanitize(string) {
-    return string.split("").filter(char => char.match(/[a-zA-Z0-9' -]/)).join("");
+    return string
+      .split("")
+      .filter((char) => char.match(/[a-zA-Z0-9' -]/))
+      .join("");
   }
-
   static titleize(string) {
-    const EXCEPTIONS = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const EXCEPTIONS = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
     const capitalizeWord = (word, i) => {
       if (i === 0) return Formatter.capitalize(word);
       else if (EXCEPTIONS.some((exception) => word === exception)) return word;
